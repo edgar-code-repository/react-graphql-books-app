9d212476579b67e4abb82de9fd9e8d77084c613a
Edgardo <ecofre@gmail.com>
2020-09-14 21:21:42 -0300

    Se agrega navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Books App</h1>
+    <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a href="#" className="navbar-brand">
+                    <i className="fas fa-book-open"></i>&nbsp;Books App
+                </a>
+            </div>
+        </div>
+    </nav> 
   );
 }
 
