f2fd4c1ab046a13871af7987fb81e8dc2f64bf35
Edgardo Cofré Durán <ecofre@gmail.com>
2020-11-04 00:04:53 -0300

    Se completa vista detalle de un libro

--- src/components/books/BookDetails.js
+++ src/components/books/BookDetails.js
@@ -8,6 +8,7 @@ const BOOK_QUERY = gql`
         book(id: $book_id) {
             title
             authors {
+                id
                 firstName
                 lastName
             }
@@ -50,20 +51,58 @@ const BookDetails = (props) => {
                 <br/>
             </div>
             <form className="book-form">
-                <div className="form-group">
-                    <input className="form-control" name="title" placeholder="Title" value={data.book.title} type="text" disabled />
-                </div>
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="row">
+                            <div className="col-md-2">
+                                <label htmlFor="title">Title:</label>
+                            </div>
+                            <div className="col-md-10">
+                                <input className="form-control" name="title" placeholder="Title" value={data.book.title} type="text" disabled />
+                            </div>
+                        </div>
+                        <div className="row">&nbsp;</div>
 
-                <div className="form-group">
-                    <input className="form-control" name="author" placeholder="Authors" type="text" disabled />
-                </div>
+                        <div className="row">
+                            <div className="col-md-2">
+                                <label htmlFor="authors">Authors:</label>
+                            </div>
+                            <div className="col-md-10">
+                            <select name="authors" multiple className="form-control">
+                            { data.book.authors.map((author) => {
+                                return <option value={author.id} key={author.id}>{author.firstName + " " + author.lastName}</option>
+                            })}                                
+                            </select>
+                            </div>                            
+                        </div>
+                        <div className="row">&nbsp;</div>
 
-                <div className="form-group">
-                    <input className="form-control" name="topic" placeholder="Topic" value={data.book.topic.name} type="text" disabled />
-                </div>
-                
-                <div className="form-group">
-                    <input className="form-control" name="imageUrl" placeholder="Image" value={data.book.imageUrl} type="text" disabled />
+                        <div className="row">
+                            <div className="col-md-2">
+                                <label htmlFor="topic">Topic:</label>
+                            </div>
+                            <div className="col-md-10">
+                                <input className="form-control" name="topic" placeholder="Topic" value={data.book.topic.name} type="text" disabled />
+                            </div>
+                        </div>
+                        <div className="row">&nbsp;</div>
+                        
+                        <div className="row">
+                            <div className="col-md-2">
+                                <label htmlFor="imageUrl">Image URL:</label>
+                            </div>
+                            <div className="col-md-10">
+                                <input className="form-control" name="imageUrl" placeholder="Image" value={data.book.imageUrl} type="text" disabled />
+                            </div>
+                        </div>
+                        <div className="row">&nbsp;</div>
+
+                    </div>
+                    <div className="col-md-4">
+                        <div className="form-group">
+                            <img className="book-details" src={data.book.imageUrl} />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="form-group">
